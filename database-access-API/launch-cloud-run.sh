a34800dfd7ee2dc34389b2ede9e85fd963064d25
Michael Salata <michael.d.salata@gmail.com>
2025-11-13 21:52:03 -0600

    added  cloudrun launch script and ignores for it

---
+++ database-access-API/launch-cloud-run.sh
@@ -0,0 +1,17 @@
+# Set your project and region
+PROJECT_ID=v58-tier3-team-37
+REGION=us-central1
+SERVICE=chingu-members-api
+IMAGE=gcr.io/$PROJECT_ID/$SERVICE:latest
+SERVICE_ACCOUNT=bq-queryer-service@v58-tier3-team-37.iam.gserviceaccount.com
+
+# Build and push the container image
+gcloud builds submit --tag $IMAGE --project $PROJECT_ID
+
+# Deploy to Cloud Run (public, HTTP)
+gcloud run deploy $SERVICE \
+  --image $IMAGE \
+  --region $REGION \
+  --project $PROJECT_ID \
+  --service-account $SERVICE_ACCOUNT \
+  --allow-unauthenticated
